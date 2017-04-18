aaa683cb0bc13ef8029aa17eb534bbc8b77bc03d
kotan.kn <kotan.kn@gmail.com>
2017-04-18 16:28:02 +0900

    Initial Commit

---
+++ YASW/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  YASW
+//
+//  Created by Kota Nakano on 2017/04/18.
+//  Copyright © 2017 Kota Nakano. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
