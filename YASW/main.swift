3217c1005aec6dfad2c4725e6c3e48aafe8b4d84
kotan.kn <kotan.kn@gmail.com>
2017-04-18 16:33:13 +0900

    [update] initial commit

--- YASW/main.swift
+++ YASW/main.swift
@@ -6,7 +6,14 @@
 //  Copyright © 2017 Kota Nakano. All rights reserved.
 //
 
-import Foundation
-
-print("Hello, World!")
-
+import Cocoa
+class Observer: NSObject {
+	func shutdown(_: NSNotification) {
+		let source: String = "tell application \"system events\" to shut down"
+		guard let script: NSAppleScript = NSAppleScript(source: source) else { fatalError("no way") }
+		script.executeAndReturnError(nil)
+	}
+}
+let observer: Observer = Observer()
+NSWorkspace.shared().notificationCenter.addObserver(observer, selector: #selector(observer.shutdown), name: .NSWorkspaceWillSleep, object: nil)
+NSApplication.shared().run()
