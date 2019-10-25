efc86166293d9c67410983930defdbe67d9ae205
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2019-10-25 18:56:53 +0900

    add log

--- YASW/main.swift
+++ YASW/main.swift
@@ -7,12 +7,19 @@
 //
 import Cocoa
 import os.log
-withExtendedLifetime(NSWorkspace.shared.notificationCenter.addObserver(forName: NSWorkspace.willSleepNotification,
-																	   object: nil,
-																	   queue: .current) {
+withExtendedLifetime(NSWorkspace.shared.notificationCenter.addObserver(forName: NSWorkspace.willSleepNotification, object: nil, queue: .current) {
 	guard let script: NSAppleScript = NSAppleScript(source: "tell application \"system events\" to shut down") else {
-		os_log("%{public}@", log: .default, type: .fault, String(describing: $0))
+		os_log(.fault, "%{public}@", String(describing: $0))
 		return
 	}
-	script.executeAndReturnError(nil)
+	do {
+		var error: NSDictionary?
+		script.executeAndReturnError(&error)
+		switch error {
+		case.none:
+			os_log(.info, "this machine will shut down by %{public}@", ProcessInfo.processInfo.processName)
+		case.some(let error):
+			os_log(.error, "@{public}@", String(describing: error))
+		}
+	}
 }, NSApplication.shared.run)
